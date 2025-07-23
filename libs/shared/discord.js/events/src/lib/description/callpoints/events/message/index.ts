6aac2244b1e6b1d1f34e4dc14963c06eef823ad7
0xar-ds <sadden-rope-sleek@duck.com>
2025-07-23 20:04:50 -0300

    feat: implement message lifecycle scope callpoints

--- libs/shared/discord.js/events/src/lib/description/callpoints/events/message/index.ts
+++ libs/shared/discord.js/events/src/lib/description/callpoints/events/message/index.ts
@@ -1,2 +1,1 @@
-// TODO: implement all events on this scope & properly set this barrel file
-export {};
+export * from './lifecycle.message-callpoints.js';
---
+++ libs/shared/discord.js/events/src/lib/description/callpoints/events/message/lifecycle.message-callpoints.ts
@@ -0,0 +1,53 @@
+import { Snowflake } from 'discord.js';
+
+import { EventCallpointMapper } from '../../event-callpoint.interface.js';
+
+type GuildId = Snowflake & {};
+type ChannelId = Snowflake & {};
+type ThreadId = Snowflake & {};
+type MessageId = Snowflake & {};
+type RecipientId = Snowflake & {};
+
+declare global {
+	interface EventCallpointMap {
+		messageCreate:
+			| `/guilds/${GuildId}/${ChannelId} ${MessageId}`
+			| `/guilds/${GuildId}/${ChannelId}/${ThreadId} ${MessageId}`
+			| `/users/${RecipientId}/${ChannelId} ${MessageId}`;
+		messageUpdate:
+			| `/guilds/${GuildId}/${ChannelId} ${MessageId}`
+			| `/guilds/${GuildId}/${ChannelId}/${ThreadId} ${MessageId}`
+			| `/users/${RecipientId}/${ChannelId} ${MessageId}`;
+		messageDelete:
+			| `/guilds/${GuildId}/${ChannelId} ${MessageId}`
+			| `/guilds/${GuildId}/${ChannelId}/${ThreadId} ${MessageId}`
+			| `/users/${RecipientId}/${ChannelId} ${MessageId}`;
+	}
+}
+
+export const messageCreate: EventCallpointMapper<'messageCreate'> = (
+	message,
+) =>
+	message.channel.isDMBased()
+		? `/users/${message.channel.recipientId}/${message.channel.id} ${message.id}`
+		: message.channel.isThread()
+			? `/guilds/${message.guild?.id ?? 'unknown'}/${message.channel.id}/${message.channel.parent?.id ?? 'unknown'} ${message.id}`
+			: `/guilds/${message.guild?.id ?? 'unknown'}/${message.channel.id} ${message.id}`;
+
+export const messageUpdate: EventCallpointMapper<'messageUpdate'> = (
+	message,
+) =>
+	message.channel.isDMBased()
+		? `/users/${message.channel.recipientId}/${message.channel.id} ${message.id}`
+		: message.channel.isThread()
+			? `/guilds/${message.guild?.id ?? 'unknown'}/${message.channel.id}/${message.channel.parent?.id ?? 'unknown'} ${message.id}`
+			: `/guilds/${message.guild?.id ?? 'unknown'}/${message.channel.id} ${message.id}`;
+
+export const messageDelete: EventCallpointMapper<'messageDelete'> = (
+	message,
+) =>
+	message.channel.isDMBased()
+		? `/users/${message.channel.recipientId}/${message.channel.id} ${message.id}`
+		: message.channel.isThread()
+			? `/guilds/${message.guild?.id ?? 'unknown'}/${message.channel.id}/${message.channel.parent?.id ?? 'unknown'} ${message.id}`
+			: `/guilds/${message.guild?.id ?? 'unknown'}/${message.channel.id} ${message.id}`;

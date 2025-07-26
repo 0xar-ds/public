248ef220cfc2286948452aae5e4a932f3f0bf617
0xar-ds <sadden-rope-sleek@duck.com>
2025-07-26 19:38:06 -0300

    feat: implement guild roles scope callpoints

---
+++ libs/shared/discord.js/events/src/lib/description/callpoints/events/guild/resources/roles.guild-scope-events.ts
@@ -0,0 +1,23 @@
+import { Snowflake } from 'discord.js';
+
+import { EventCallpointMapper } from '../../../event-callpoint.interface.js';
+
+type GuildId = Snowflake & {};
+type RoleId = Snowflake & {};
+
+declare global {
+	interface EventCallpointMap {
+		roleCreate: `/guilds/${GuildId}/roles ${RoleId}`;
+		roleUpdate: `/guilds/${GuildId}/roles ${RoleId}`;
+		roleDelete: `/guilds/${GuildId}/roles ${RoleId}`;
+	}
+}
+
+export const roleCreate: EventCallpointMapper<'roleCreate'> = (role) =>
+	`/guilds/${role.guild.id}/roles ${role.id}`;
+
+export const roleUpdate: EventCallpointMapper<'roleUpdate'> = (_, role) =>
+	`/guilds/${role.guild.id}/roles ${role.id}`;
+
+export const roleDelete: EventCallpointMapper<'roleDelete'> = (role) =>
+	`/guilds/${role.guild.id}/roles ${role.id}`;

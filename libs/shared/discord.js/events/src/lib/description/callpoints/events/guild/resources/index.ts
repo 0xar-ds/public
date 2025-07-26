45b2d90d2652abc3eadcc5b363926c6f73876d1d
0xar-ds <sadden-rope-sleek@duck.com>
2025-07-26 20:17:34 -0300

    feat: implement guild expressions scope callpoints

--- libs/shared/discord.js/events/src/lib/description/callpoints/events/guild/resources/index.ts
+++ libs/shared/discord.js/events/src/lib/description/callpoints/events/guild/resources/index.ts
@@ -2,3 +2,4 @@ export * from './events.guild-scope-events.js';
 export * from './invites.guild-scope-events.js';
 export * from './logs.guild-scope-events.js';
 export * from './roles.guild-scope-events.js';
+export * from './expressions.guild-scope-events.js';

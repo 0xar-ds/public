f0432e97436795cf051cf8a3888d46ea73611c30
0xar-ds <sadden-rope-sleek@duck.com>
2025-07-26 19:30:23 -0300

    feat: implement guild invites scope callpoints

---
+++ libs/shared/discord.js/events/src/lib/description/callpoints/events/guild/resources/invites.guild-scope-events.ts
@@ -0,0 +1,28 @@
+import { Snowflake } from 'discord.js';
+
+import { EventCallpointMapper } from '../../../event-callpoint.interface.js';
+
+type GuildId = Snowflake & {};
+type ChannelId = Snowflake & {};
+type InviteCode = string & {};
+
+declare global {
+	interface EventCallpointMap {
+		inviteCreate:
+			| `/guilds/${GuildId}/invites ${InviteCode}`
+			| `/groups/${ChannelId}/invites ${InviteCode}`;
+		inviteDelete:
+			| `/guilds/${GuildId}/invites ${InviteCode}`
+			| `/groups/${ChannelId}/invites ${InviteCode}`;
+	}
+}
+
+export const inviteCreate: EventCallpointMapper<'inviteCreate'> = (invite) =>
+	invite.guild !== null
+		? `/guilds/${invite.guild.id}/invites ${invite.code}`
+		: `/groups/${invite.channelId ?? 'unknown'}/invites ${invite.code}`;
+
+export const inviteDelete: EventCallpointMapper<'inviteDelete'> = (invite) =>
+	invite.guild !== null
+		? `/guilds/${invite.guild.id}/invites ${invite.code}`
+		: `/groups/${invite.channelId ?? 'unknown'}/invites ${invite.code}`;

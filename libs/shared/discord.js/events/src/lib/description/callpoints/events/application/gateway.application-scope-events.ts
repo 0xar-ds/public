558220860c5a3827a8ed104195c1f4740e6300d1
0xar-ds <sadden-rope-sleek@duck.com>
2025-07-25 22:03:19 -0300

    feat: implement application gateway scope callpoints

---
+++ libs/shared/discord.js/events/src/lib/description/callpoints/events/application/gateway.application-scope-events.ts
@@ -0,0 +1,66 @@
+import { Snowflake } from 'discord.js';
+
+import {
+	getInteractionTypeKey,
+	InteractionTypeKey,
+} from '@~discord.js/interactions';
+
+import { EventCallpointMapper } from '../../event-callpoint.interface.js';
+
+type ShardId = number & {};
+type InteractionId = string & {};
+type UserId = Snowflake & {};
+
+declare global {
+	interface EventCallpointMap {
+		ready: `/gateway/client`;
+		error: `/gateway/client`;
+		warn: `/gateway/client`;
+		debug: `/gateway/client`;
+
+		shardReady: `/gateway/client/shards ${ShardId}`;
+		shardError: `/gateway/client/shards ${ShardId}`;
+		shardResume: `/gateway/client/shards ${ShardId}`;
+		shardDisconnect: `/gateway/client/shards ${ShardId}`;
+		shardReconnecting: `/gateway/client/shards ${ShardId}`;
+
+		interactionCreate: `/gateway/interactions/${InteractionTypeKey} ${InteractionId}`;
+		presenceUpdate: `/gateway/presences ${UserId}`;
+	}
+}
+
+export const ready: EventCallpointMapper<'ready'> = (_) => `/gateway/client`;
+
+export const error: EventCallpointMapper<'error'> = (_) => `/gateway/client`;
+
+export const warn: EventCallpointMapper<'warn'> = (_) => `/gateway/client`;
+
+export const debug: EventCallpointMapper<'debug'> = (_) => `/gateway/client`;
+
+export const shardReady: EventCallpointMapper<'shardReady'> = (shard, _) =>
+	`/gateway/client/shards ${shard}`;
+
+export const shardError: EventCallpointMapper<'shardError'> = (_, shard) =>
+	`/gateway/client/shards ${shard}`;
+
+export const shardResume: EventCallpointMapper<'shardResume'> = (shard, _) =>
+	`/gateway/client/shards ${shard}`;
+
+export const shardDisconnect: EventCallpointMapper<'shardDisconnect'> = (
+	_,
+	shard,
+) => `/gateway/client/shards ${shard}`;
+
+export const shardReconnecting: EventCallpointMapper<'shardReconnecting'> = (
+	shard,
+) => `/gateway/client/shards ${shard}`;
+
+export const interactionCreate: EventCallpointMapper<'interactionCreate'> = (
+	interaction,
+) =>
+	`/gateway/interactions/${getInteractionTypeKey(interaction.type)} ${interaction.id}`;
+
+// TODO: consider /gateway/presences GuildId/GuildMemberId/UserId
+export const presenceUpdate: EventCallpointMapper<'presenceUpdate'> = (
+	presence,
+) => `/gateway/presences ${presence?.userId ?? 'unknown'}`;
--- libs/shared/discord.js/events/src/lib/description/callpoints/events/application/index.ts
+++ libs/shared/discord.js/events/src/lib/description/callpoints/events/application/index.ts
@@ -1,1 +1,2 @@
 export * from './client.application-scope-events.js';
+export * from './gateway.application-scope-events.js';

e203968f2fad4b001afd9c80e745c8a41f716713
0xar-ds <sadden-rope-sleek@duck.com>
2025-07-26 20:45:59 -0300

    fix: properly name callpoints scope files

--- libs/shared/discord.js/events/src/lib/description/callpoints/events/guild/index.ts
+++ libs/shared/discord.js/events/src/lib/description/callpoints/events/guild/index.ts
@@ -1,4 +1,4 @@
-export * from './configuration.guild-scope-events.js';
-export * from './members.guild-scope-events.js';
+export * from './configuration.guild-scope-callpoints.js';
+export * from './members.guild-scope-callpoints.js';
 
 export * from './resources/index.js';

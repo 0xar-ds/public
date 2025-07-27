2ad3bf523d3085a71614f17478f1e307304f6d99
0xar-ds <sadden-rope-sleek@duck.com>
2025-07-27 01:30:25 -0300

    feat: add categoryid to threadMemberUpdate callpoint

--- libs/shared/discord.js/events/src/lib/description/callpoints/events/application/client.application-scope-callpoints.ts
+++ libs/shared/discord.js/events/src/lib/description/callpoints/events/application/client.application-scope-callpoints.ts
@@ -4,6 +4,7 @@ import { EventCallpointMapper } from '../../event-callpoint.interface.js';
 
 type GuildId = Snowflake & {};
 type ParentId = Snowflake & {};
+type CategoryId = Snowflake & {};
 type MemberId = Snowflake & {};
 type ThreadId = Snowflake & {};
 type Nonce = Snowflake & {};
@@ -16,7 +17,7 @@ declare global {
 		guildMemberAvailable: `/client/guilds/${GuildId}/members ${MemberId}`;
 		guildMembersChunk: `/client/guilds/${GuildId}/members ${Nonce}`;
 
-		threadMemberUpdate: `/client/guilds/${GuildId}/${ParentId}/${ThreadId}`;
+		threadMemberUpdate: `/client/guilds/${GuildId}/${CategoryId}/${ParentId}/${ThreadId}`;
 
 		threadListSync: `/client/guilds/${GuildId}/threads`;
 		soundboardSounds: `/client/guilds/${GuildId}/sounds`;
@@ -48,7 +49,7 @@ export const threadMemberUpdate: EventCallpointMapper<'threadMemberUpdate'> = (
 	_,
 	member,
 ) =>
-	`/client/guilds/${member.thread.guild.id}/${member.thread.parent?.id ?? 'unknown'}/${member.thread.id}`;
+	`/client/guilds/${member.thread.guildId}/${member.thread.parent?.parentId ?? 'UNKNOWN_CATEGORY'}/${member.thread.parentId ?? 'UNKNOWN_CHANNEL'}/${member.thread.id}`;
 
 export const threadListSync: EventCallpointMapper<'threadListSync'> = (
 	_,

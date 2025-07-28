14a5287ad5a53410e977e0648253397a1cad0625
0xar-ds <sadden-rope-sleek@duck.com>
2025-07-28 12:11:28 -0300

    feat(descriptions/body): add thread scope body parsers

--- libs/shared/discord.js/events/src/lib/description/body/events/index.ts
+++ libs/shared/discord.js/events/src/lib/description/body/events/index.ts
@@ -1,4 +1,4 @@
-// export * from './thread/index.js';
+export * from './thread/index.js';
 export * from './message/index.js';
 // export * from './guild/index.js';
 export * from './channel/index.js';

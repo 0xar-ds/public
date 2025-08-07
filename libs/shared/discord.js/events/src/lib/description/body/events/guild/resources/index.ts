fb418e64a0b6aa1a8a5b51ec540f7777292dce29
0xar-ds <sadden-rope-sleek@duck.com>
2025-08-07 00:18:56 -0300

    feat(descriptions/body): add guild members & audit logs scope body parsers

---
+++ libs/shared/discord.js/events/src/lib/description/body/events/guild/resources/index.ts
@@ -0,0 +1,5 @@
+// export * from './events.guild-scope-body.js';
+// export * from './invites.guild-scope-body.js';
+export * from './logs.guild-scope-body.js';
+// export * from './roles.guild-scope-body.js';
+// export * from './expressions.guild-scope-body.js';

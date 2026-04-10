250eade3a3f600d56f7d282c820f0e0528b3b689
0xar-ds <sadden-rope-sleek@duck.com>
2026-04-10 13:45:16 -0300

    feat: allow for override of message in exceptions
    Signed-off-by: 0xar-ds <sadden-rope-sleek@duck.com>

--- libs/server/discord/necord-enhancements/src/lib/exceptions/forbidden.exception.ts
+++ libs/server/discord/necord-enhancements/src/lib/exceptions/forbidden.exception.ts
@@ -1,12 +1,17 @@
 import { Status } from '@~server/core-api';
 
 import { NecordException } from './necord.exception.ts';
+import { Nullable } from '../../../../../../../types/utils/utils.ts';
 import { GatewayResponseType, PayloadOf } from '../response/index.ts';
 
 export class ForbiddenException<
 	T extends GatewayResponseType,
 > extends NecordException<T> {
-	constructor(type: T, payload: PayloadOf<T>) {
-		super(type, Status.UNAHOTIRZED_ERROR, payload);
+	constructor(
+		type: T,
+		payload: Nullable<PayloadOf<T>> = null,
+		overrideMessage?: string,
+	) {
+		super(type, Status.UNAHOTIRZED_ERROR, payload, overrideMessage);
 	}
 }
--- libs/server/discord/necord-enhancements/src/lib/exceptions/necord.exception.ts
+++ libs/server/discord/necord-enhancements/src/lib/exceptions/necord.exception.ts
@@ -2,6 +2,8 @@ import { IntrinsicException } from '@nestjs/common';
 
 import { StatusDescription } from '@~server/core-api';
 
+import { Nullable } from '../../../../../../../types/utils/utils.ts';
+
 import {
 	GatewayResponseLike,
 	GatewayResponseType,
@@ -9,32 +11,48 @@ import {
 	PayloadOf,
 } from '../response/index.ts';
 
+export type UnresolvedGatewayResponse<
+	T extends GatewayResponseType = GatewayResponseType,
+> = Omit<GatewayResponseLike<T>, 'payload'> & {
+	payload: Nullable<PayloadOf<T>>;
+};
+
 export class NecordException<
 	T extends GatewayResponseType,
 > extends IntrinsicException {
-	private readonly _response: GatewayResponseLike;
+	private readonly _response: UnresolvedGatewayResponse<T>;
 
-	constructor(response: GatewayResponseLike);
-	constructor(type: T, status: StatusDescription, payload: PayloadOf<T>);
+	constructor(response: GatewayResponseLike<T>);
 	constructor(
-		typeOrResponse: T | GatewayResponseLike,
+		type: T,
+		status: StatusDescription,
+		payload?: Nullable<PayloadOf<T>>,
+		overrideMessage?: string,
+	);
+	constructor(
+		typeOrResponse: T | GatewayResponseLike<T>,
 		status?: StatusDescription,
-		payload?: PayloadOf<T>,
+		payload?: Nullable<PayloadOf<T>>,
+		overrideMessage?: string,
 	) {
 		super();
 
 		if (isGatewayResponseLike(typeOrResponse)) {
-			this._response = typeOrResponse;
+			this._response = {
+				...typeOrResponse,
+				payload: typeOrResponse.payload ?? null,
+			};
 		} else {
 			this._response = {
 				type: typeOrResponse,
-				status: status!,
-				payload: payload!,
+				status: overrideMessage
+					? { ...status!, message: overrideMessage }
+					: status!,
+				payload: payload ?? null,
 			};
 		}
 	}
-
-	toResponse(): GatewayResponseLike {
+	toResponse(): UnresolvedGatewayResponse<T> {
 		return this._response;
 	}
 }
